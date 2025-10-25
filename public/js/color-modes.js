530f0aae8a563f55223fe7e2a5221103510de954
WoCha-FR <wocha@wocha.fr>
2025-10-26 00:30:42 +0200

    rfids for admin

--- public/js/color-modes.js
+++ public/js/color-modes.js
@@ -6,19 +6,15 @@
 
 (() => {
   'use strict'
-
   const getStoredTheme = () => localStorage.getItem('theme')
   const setStoredTheme = theme => localStorage.setItem('theme', theme)
-
   const getPreferredTheme = () => {
     const storedTheme = getStoredTheme()
     if (storedTheme) {
       return storedTheme
     }
-
     return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
   }
-
   const setTheme = theme => {
     if (theme === 'auto') {
       document.documentElement.setAttribute('data-bs-theme', (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'))
@@ -26,47 +22,46 @@
       document.documentElement.setAttribute('data-bs-theme', theme)
     }
   }
-
   setTheme(getPreferredTheme())
-
   const showActiveTheme = (theme, focus = false) => {
     const themeSwitcher = document.querySelector('#bd-theme')
 
     if (!themeSwitcher) {
       return
     }
-
     const themeSwitcherText = document.querySelector('#bd-theme-text')
     const activeThemeIcon = document.querySelector('.theme-icon-active use')
     const btnToActive = document.querySelector(`[data-bs-theme-value="${theme}"]`)
     const svgOfActiveBtn = btnToActive.querySelector('svg use').getAttribute('href')
-
     document.querySelectorAll('[data-bs-theme-value]').forEach(element => {
       element.classList.remove('active')
       element.setAttribute('aria-pressed', 'false')
     })
-
     btnToActive.classList.add('active')
     btnToActive.setAttribute('aria-pressed', 'true')
     activeThemeIcon.setAttribute('href', svgOfActiveBtn)
     const themeSwitcherLabel = `${themeSwitcherText.textContent} (${btnToActive.dataset.bsThemeValue})`
     themeSwitcher.setAttribute('aria-label', themeSwitcherLabel)
-
     if (focus) {
       themeSwitcher.focus()
     }
+    // flatpickr
+    const stylesheet = document.head.querySelector('link[href^="/css/flatpickr"]');
+    const newtheme = document.documentElement.getAttribute('data-bs-theme');
+    if (newtheme == 'dark') {
+      stylesheet.href = '/css/flatpickr-dark.min.css';
+    } else {
+      stylesheet.href = '/css/flatpickr.min.css';
+    }
   }
-
   window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
     const storedTheme = getStoredTheme()
     if (storedTheme !== 'light' && storedTheme !== 'dark') {
       setTheme(getPreferredTheme())
     }
   })
-
   window.addEventListener('DOMContentLoaded', () => {
     showActiveTheme(getPreferredTheme())
-
     document.querySelectorAll('[data-bs-theme-value]')
       .forEach(toggle => {
         toggle.addEventListener('click', () => {
