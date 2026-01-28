6d764931d9f7d51f77da1702ffd8a5f13c11230c
WoCha-FR <wocha@wocha.fr>
2026-01-28 21:24:51 +0100

    Nouveau module de configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,22 @@
+import { defineConfig } from "eslint/config";
 import pluginJs from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
+import eslintPluginJsonc from 'eslint-plugin-jsonc';
+import eslintPluginJsonSchemaValidator from "eslint-plugin-json-schema-validator";
 
-/** @type {import('eslint').Linter.Config[]} */
-export default [
+export default defineConfig([
   pluginJs.configs.recommended,
   eslintConfigPrettier,
+  eslintPluginJsonc.configs["flat/recommended-with-json5"],
+  eslintPluginJsonSchemaValidator.configs.recommended,
   {
     ignores: ['web/public/js/*'],
   },
-];
+  {
+    files: ['*.json5'],
+    plugins: {
+      jsonc: eslintPluginJsonc,
+    },
+    language: 'json/json5',
+  }
+]);
